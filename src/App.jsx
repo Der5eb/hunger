ade0d262177adce3926cb4e347b62affc65dac66
Sebastian Meger <sebastianmeger@Mac.fritz.box>
2026-04-11 10:09:08 +0200

    fix: Passwort Recovery

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import { supabase } from './lib/supabase'
+import { AuthProvider } from './context/AuthContext'
 import Navigation from "./components/Navigation";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
